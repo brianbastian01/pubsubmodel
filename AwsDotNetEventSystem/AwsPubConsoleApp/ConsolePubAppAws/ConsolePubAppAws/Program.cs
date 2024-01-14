d3db255440e9afbee561bf3a181f9649a6e87da0
brianbastian01 <80248367+brianbastian01@users.noreply.github.com>
2024-01-14 15:12:49 +0700

    Initial Commit

---
+++ AwsDotNetEventSystem/AwsPubConsoleApp/ConsolePubAppAws/ConsolePubAppAws/Program.cs
@@ -0,0 +1,19 @@
+﻿using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var snsClient = new AmazonSimpleNotificationServiceClient(RegionEndpoint.USEast2);
+        var topicArn = "YOUR_TOPIC_ARN";
+        var message = "Hello, this is a message from the publisher.";
+
+        var request = new PublishRequest(topicArn, message);
+
+        var response = await snsClient.PublishAsync(request);
+
+        Console.WriteLine($"Message sent with message ID: {response.MessageId}");
+    }
+}
---
+++ AwsDotNetEventSystem/AwsSubConsoleApp/ConsoleSubAppAws/ConsoleSubAppAws/Program.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var snsClient = new AmazonSimpleNotificationServiceClient(RegionEndpoint.YOUR_REGION);
+        var sqsClient = new AmazonSQSClient(RegionEndpoint.YOUR_REGION);
+
+        var queueUrl = "YOUR_QUEUE_URL";
+
+        var subscribeRequest = new SubscribeRequest
+        {
+            Protocol = "sqs",
+            Endpoint = queueUrl,
+            TopicArn = "YOUR_TOPIC_ARN"
+        };
+
+        var subscribeResponse = await snsClient.SubscribeAsync(subscribeRequest);
+
+        Console.WriteLine($"Subscribed to topic with subscription ARN: {subscribeResponse.SubscriptionArn}");
+
+        while (true)
+        {
+            var receiveMessageRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrl,
+                MaxNumberOfMessages = 1
+            };
+
+            var receiveMessageResponse = await sqsClient.ReceiveMessageAsync(receiveMessageRequest);
+
+            foreach (var message in receiveMessageResponse.Messages)
+            {
+                Console.WriteLine($"Received message: {message.Body}");
+
+                // Process the message as needed
+
+                // Delete the message from the queue
+                await sqsClient.DeleteMessageAsync(queueUrl, message.ReceiptHandle);
+            }
+
+            Thread.Sleep(1000); // Add a delay to avoid constant polling
+        }
+    }
+}
